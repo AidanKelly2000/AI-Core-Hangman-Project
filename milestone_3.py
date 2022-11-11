026977e1787234543699a9289eac88eb41d19284
Aidan Kelly <ak2344@hotmail.co.uk>
2022-11-11 16:49:13 +0000

    Adds an if statement to work out if the guess is in the word or not, prints the letter guessed

--- milestone_3.py
+++ milestone_3.py
@@ -1,4 +1,12 @@
+import random
+
 # Creating while loop so the user will be continually asked for more letters
+word_list = ["mangoes", "bananas", "grapes", "strawberries", "oranges"]
+print(word_list)
+
+# Selects a random word from the list of fruits "word_list", and prints it to terminal
+word = random.choice(word_list)
+print(word)
 
 while True:
     guess = input("Please enter a single letter: ")
@@ -6,4 +14,10 @@ while True:
         break
     else:
         print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
+
 
