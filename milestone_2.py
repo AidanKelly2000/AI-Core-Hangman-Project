d11f160f28d297f2eaf7c814a5e6c2e5ffa746ee
Aidan Kelly <ak2344@hotmail.co.uk>
2022-11-11 15:23:55 +0000

    Adds an input for the user to guess a letter. Creates an if statement to determine if the input is valid

--- milestone_2.py
+++ milestone_2.py
@@ -3,12 +3,26 @@ import random
 # Creating a list of favourite fruits and assigning to the variable word_list
 
 word_list = ["Mangoes", "Bananas", "Grapes", "Strawberries", "Oranges"]
-
 print(word_list)
 
-word = random.choice(word_list)
+# Selects a random word from the list of fruits "word_list", and prints it to terminal
 
+word = random.choice(word_list)
 print(word)
+
+guess = input("Please enter a single letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
+
+
+
+
+
+
+
 
 
 
