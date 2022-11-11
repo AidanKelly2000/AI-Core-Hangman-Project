7fd76d92823e49044a88818e1ddb28eeb17f39e8
Aidan Kelly <ak2344@hotmail.co.uk>
2022-11-11 15:11:11 +0000

    Adds the import 'random' and prints a random word from the list

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,14 @@
+import random
+
 # Creating a list of favourite fruits and assigning to the variable word_list
 
 word_list = ["Mangoes", "Bananas", "Grapes", "Strawberries", "Oranges"]
 
 print(word_list)
+
+word = random.choice(word_list)
+
+print(word)
 
 
 
