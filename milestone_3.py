2c4fe849c8d5f3e03af14e851744a159ffa25029
Aidan Kelly <ak2344@hotmail.co.uk>
2022-11-12 15:25:32 +0000

    Adds the functions to ask for an input iteratively, and check if the guess is correct

--- milestone_3.py
+++ milestone_3.py
@@ -7,7 +7,7 @@ print(word_list)
 # Selects a random word from the list of fruits "word_list", and prints it to terminal
 word = random.choice(word_list)
 print(word)
-
+"""
 while True:
     guess = input("Please enter a single letter: ")
     if len(guess) == 1 and guess.isalpha():
@@ -19,5 +19,21 @@ if guess in word:
     print(f"Good guess! {guess} is in the word.")
 else:
     print(f"Sorry, {guess} is not in the word. Try again.")
+"""
 
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
+def ask_for_input():
+    while True:
+        guess = input("Please enter a single letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
+ask_for_input()
