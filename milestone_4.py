dcac9a5efd0f538b052c5a99d7261cb535681651
Aidan Kelly <ak2344@hotmail.co.uk>
2022-11-13 20:00:05 +0000

    Adds class Hangman and initialised attributes

---
+++ milestone_4.py
@@ -0,0 +1,12 @@
+import random
+
+class Hangman:
+
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.word = random.choice(self.word_list)
+        self.word_guessed = ['_' for letters in range(len(self.word))]
+        self.num_letters = len(self.word)
+        self.list_of_guesses = []
+        word_list = ["mangoes", "bananas", "grapes", "strawberries", "oranges"]
