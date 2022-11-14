fb463d5679760b335edc1337cde798592e113b1e
Aidan Kelly <ak2344@hotmail.co.uk>
2022-11-14 13:48:17 +0000

    Changed the if statement in ask_for_input, it now runs using or and False. inserted list into intialiser

--- milestone_4.py
+++ milestone_4.py
@@ -2,15 +2,13 @@ import random
 
 class Hangman:
 
-    def __init__(self, word_list, num_lives=5):
+    def __init__(self, word_list = ["mangoes", "bananas", "grapes", "strawberries", "oranges"], num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = random.choice(self.word_list)
         self.word_guessed = ['_' for x in range(len(self.word))]
         self.num_letters = len(self.word)
         self.list_of_guesses = []
-        word_list = ["mangoes", "bananas", "grapes", "strawberries", "oranges"]
-        # does putting a list in the initialiser work? 
 
     def check_guess(self, guess):
         guess = guess.lower()
@@ -35,17 +33,18 @@ class Hangman:
 
         while True:
             guess = input("Please enter a single letter: ")
-            if len(guess) != 1 and guess != guess.isalpha():   # How do i get it so that the if statement works
+            if len(guess) != 1 or guess.isalpha() == False:
                 print("Invalid letter. Please, enter a single alphabetical character.")
                 
             elif guess in self.list_of_guesses:
                 print("You already tried that letter!")
 
             else:
-                game.check_guess(guess)    # Is this running correct
+                game.check_guess(guess)
                 self.list_of_guesses += [guess]
 
 
-game = Hangman
+game = Hangman()  # if you pass a new list and number of lives in the parenthesis of this object's(game) class, it will overwrite the pre defined parameters in the classes initialiser 
 game.ask_for_input()
 
+""" need to edit step 3 of milestone 4"""
