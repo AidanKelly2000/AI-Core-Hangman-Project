79196fbb38e1f2e044b332560d393aa90b11e62e
Aidan Kelly <ak2344@hotmail.co.uk>
2022-11-14 21:53:09 +0000

    adds breaks to play_game, uses indexing to replace guess in the random word. Hangman game is complete!

--- milestone_5.py
+++ milestone_5.py
@@ -7,7 +7,7 @@ class Hangman:
         self.num_lives = num_lives
         self.word = random.choice(self.word_list)
         self.word_guessed = ['_' for x in range(len(self.word))]
-        self.num_letters = len(self.word)
+        self.num_letters = len(set(self.word))
         self.list_of_guesses = []
 
     def check_guess(self, guess):
@@ -15,12 +15,11 @@ class Hangman:
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
 
-            for letter in self.word:
-                if letter == guess:
-                    position = self.word.find(letter)
-                    print(position)
-                    self.word_guessed[position] = letter
-                    print(self.word_guessed)
+            for idx in range(0, len(self.word_guessed)):
+                if guess == self.word[idx]:
+                    self.word_guessed[idx] = guess
+                    
+            print(self.word_guessed)
             self.num_letters -= 1
 
         else:
@@ -45,17 +44,22 @@ class Hangman:
                 print(self.word_guessed)
                 self.check_guess(guess)
                 self.list_of_guesses += [guess]
+                break
 
 def play_game():
     game = Hangman(["cherries", "bananas", "figs", "strawberries", "blueberries"], 7)
 
     while True:
+
         if game.num_lives == 0:
             print("You lost!")
+            break
         elif game.num_letters > 0:
             game.ask_for_input()
+            
         else:
             print("Congratulations. You won the game!")
+            break
 
 
 
